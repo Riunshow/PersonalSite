80c875013a0c48c24ee178b7631663e540d1f42f
Riunshow <zhubotaigg@gmail.com>
2018-03-20 21:36:04 +0800

    实现颜色随时间改变

---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		
+	</div>
+</template>
+
+<script>
+export default {
+  name: "component_name",
+  data () {
+	return {
+	};
+  }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
